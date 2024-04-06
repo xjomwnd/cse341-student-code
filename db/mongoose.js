30ca8577b1c1981b73384da4faa837eabc625142
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-07 01:07:06 +0300

    commit

---
+++ db/mongoose.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+// Database connection URI
+const DB_URI = 'mongodb+srv://finalproject:7Thayowaku77@cluster0.hbwhk.mongodb.net/cse341-Final'; 
+
+// Connect to MongoDB
+mongoose.connect(DB_URI);
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Event listeners for Mongoose connection
+db.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+db.on('error', (err) => {
+  console.error('Error connecting to MongoDB:', err.message);
+});
+
+db.on('disconnected', () => {
+  console.log('Disconnected from MongoDB');
+});
+
+// Gracefully close MongoDB connection when Node.js process terminates
+process.on('SIGINT', async () => {
+  try {
+    await mongoose.connection.close();
+    console.log('MongoDB connection closed');
+    process.exit(0);
+  } catch (err) {
+    console.error('Error closing MongoDB connection:', err.message);
+    process.exit(1);
+  }
+});
+
+module.exports = db;
