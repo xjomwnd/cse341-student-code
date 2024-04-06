c7948ce10654f255b46a458940e7aab0a3449a4f
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-07 02:10:51 +0300

    commit

---
+++ models/express-session.js
@@ -0,0 +1,19 @@
+// express-sessions.js
+
+const express = require('express');
+const session = require('express-session'); // Correct import statement
+// eslint-disable-next-line no-unused-vars
+
+
+// Create a new Express application
+const app = express();
+
+// Set up express-session middleware
+app.use(session({
+  secret: 'your-secret-key', // Change this to your secret key
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Export the configured session middleware
+module.exports = app;
