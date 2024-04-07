cea7ff2846a036fbe7fe6bd5fd05ab6a62729b5c
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-07 04:10:42 +0300

    commit

--- validator.js
+++
@@ -1,6 +0,0 @@
-const customValidator = require('./validator');
-
-// Example usage
-console.log(customValidator.isAdult(20)); // true
-console.log(validator.isEmail('example@example.com')); // true
-console.log(validator.isStrongPassword('password123')); // false
