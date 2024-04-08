4716e50add4a069a4ee31e20c4a8f062291e786b
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-08 09:51:35 +0300

    commit

--- frontend/js/submission.js
+++ frontend/js/submission.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
 document.getElementById("addTextForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent default form submission
     
     // Retrieve the text input value
+    // eslint-disable-next-line no-unused-vars
     var text = document.getElementById("textInput").value;
     
     // Perform any actions with the entered text (e.g., add it to a list, send it to the server, etc.)
