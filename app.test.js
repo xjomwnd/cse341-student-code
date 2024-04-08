632473f090be88cc5e6f5209df09e86c1d42ef10
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-08 20:16:11 +0300

    COMMIT

--- app.test.js
+++ app.test.js
@@ -1,10 +1,19 @@
 const request = require('supertest');
-const app = require('./app');
+const app = require('./app'); // Assuming your Express app is in app.js
 
 describe('GET /api/posts', () => {
-    });
+  test('It should respond with status 200', async () => {
+    const response = await request(app).get('/api/posts');
+    expect(response.status).toBe(200);
+  });
 
-   test('It should respond with status 200', async () => {
-        const response = await request(app).get('/api/posts');
-        expect(response.status).toBe(200);
-   });
+  test('It should respond with JSON', async () => {
+    const response = await request(app).get('/api/posts');
+    expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  test('It should respond with an array of posts', async () => {
+    const response = await request(app).get('/api/posts');
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+});
