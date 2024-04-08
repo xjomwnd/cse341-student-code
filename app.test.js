ac69cb504f4354e8aba7e9a9944e7baf7096099f
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-08 20:02:04 +0300

    commit

--- app.test.js
+++ app.test.js
@@ -3,8 +3,7 @@ const app = require('./app');
 
 describe('GET /api/posts', () => {
     });
-});
+
    test('It should respond with status 200', async () => {
         const response = await request(app).get('/api/posts');
         expect(response.status).toBe(200);
- 
